cfa4e93aaf5d4bbcec2b062871941db2fa6bf600
vmolash <molash.v@gmail.com>
2020-03-04 11:12:10 -0800

    switch

--- script/script.js
+++ script/script.js
@@ -1,22 +1,59 @@
-let num = parseInt(window.prompt('Enter a number between 1 and 10'));
-window.console.log(typeof num);
+// let num = parseInt(window.prompt('Enter a number between 1 and 10'));
+// window.console.log(typeof num);
 // if (num === 5) {
 // window.document.write('You guessed right!');
 // } else {
 //     window.document.write('You guessed wrong number!');
 // }
 
-(num === 5) ? window.document.write('You guessed right!') : window.document.write('You guessed wrong number!');
+// (num === 5) ? window.document.write('You guessed right!') : window.document.write('You guessed wrong number!');
 
-let grade = window.prompt('Enter the student\'s grade');
-if (grade >= '90') {
-    window.document.write('Student received an A');
-} else if (grade >=80) {
-    window.document.write('Student received an B');
-} else if (grade >=70) {
-    window.document.write('Student received an C');
-} else if (grade >=60) {
-    window.document.write('Student received an D');
-} else {
-    window.document.write('Student failed the course');
+// let grade = window.prompt('Enter the student\'s grade');
+// if (grade >= '90') {
+//     window.document.write('Student received an A');
+// } else if (grade >=80) {
+//     window.document.write('Student received an B');
+// } else if (grade >=70) {
+//     window.document.write('Student received an C');
+// } else if (grade >=60) {
+//     window.document.write('Student received an D');
+// } else {
+//     window.document.write('Student failed the course');
+// }
+
+
+// nested 
+// let discount = 0;
+// let total = window.prompt('Enter cart total');
+// let type = window.prompt('Enter customer type (r/w');
+
+// if (type === 'r') {
+//     if (total > 500) {
+//         discount = .10;
+//     } else if (total > 100) {
+//         discount = .05;
+//     }
+// } else if (type === 'w') {
+//     if (total > 500) {
+//         discount = .20;
+//     } else if (total > 100) {
+//         discount = .10;
+//     }
+// } else {
+//     discount = 0;
+// }
+
+// window.document.write('Customer type: ', type, '<br>');
+// window.document.write('Discount applied: ', discount);
+
+let grade = window.prompt('Enter the student\s grade');
+switch (grade) {
+    case 'A':
+        window.document.write('You got an A!');
+        break;
+    case 'B':
+        window.document.write('Your got an B!');
+        break;
+    default:
+        window.document.write('Your credits won\'t articulate!');
 }
