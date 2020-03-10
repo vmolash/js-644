4dc8f80283da3191a921d51d56eaa9fe68570a5d
vmolash <molash.v@gmail.com>
2020-03-10 13:04:46 -0700

    assignment1

---
+++ script.js
@@ -0,0 +1,137 @@
+// STET 1
+// var num = 8; // numeric
+// var str = "hello"; // string
+// var bool = true; // boolean true or false
+// console.log(num, str, bool);
+
+// STEP 2
+// var strFirstName;
+// var strLastName;
+// var strAddress;
+// var strCity;
+// var strState;
+// var strZipCode;
+// var numYourAge;
+// var strReferralSource;
+// var boolMayWeContactYou;
+
+// STEP 3
+// 1 method - Declare many variables in one statement
+// var strFirstName = "Mike", numYourAge = 45, boolMayWeContactYou = true;
+// console.log(strFirstName, numYourAge, boolMayWeContactYou);
+
+// 2 method - Declare a variable is one statement (after the declaration, the variable has no value (undefined)). Assign a value to the variable is a second statement. 
+// var strLastName;
+// strLastName = "Jonhson";
+// console.log(strLastName);
+
+// var strAddress;
+// strAddress = "8330 Aero drive";
+// console.log(strAddress);
+
+// 3 method - Assign a value to the variable when we declare it
+// var strCity = "San Diego";
+// var strZipCode = "92104";
+// var strReferralSource = "Bob Marley"
+// var boolMayWeContactYou = false;
+// console.log(strCity, strZipCode, strReferralSource, boolMayWeContactYou);
+
+// STEP 4
+// var str = "I like to eat apples when it is sunny and not raining.";
+// console.log("The sentence has " +  str.length + " characters");
+
+// STEP 5
+// var first = "card";
+// console.log(first + " " + 7 + " is on the table");
+
+// var second = "Is it";
+// console.log(second + " " + true + " that Pluto is not a planet?!");
+
+// var third = 3.141592653589793238462643;
+// console.log(Math.round(third));
+
+// STEP 6
+// var heading = "Let's make this earth cleaner";
+// var paragr = " Join us at 9am every Sunday Pacific Beach";
+// document.write("<h1>" + heading + "</h1>" + "<p>" + paragr + "</p>");
+
+// STEP 7
+// It is not valid. Fixed variant below
+
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// console.log(someString);
+
+
+// STEP 8
+// Print to the console
+
+// function createUserName() {
+//     var userName = prompt("Enter your name");
+//     console.log("Welcome " + userName + " to 644 class!");
+// }
+// createUserName();
+
+// Print to the page
+
+// function createUserName(name) {
+//     var hEle = document.createElement('h2');
+//     var nNode = document.createTextNode("Welcome " + name + " to our page!");
+//     hEle.appendChild(nNode);
+//     document.body.appendChild(hEle);
+// }
+// createUserName(prompt("Please Enter you name"));
+
+// STEP 9
+// console.log(typeof("forest"));
+// console.log(typeof(4));
+// console.log(typeof(true));
+
+// console.log(typeof([4, 2, 'five']));
+// console.log(typeof({name: "Bob", age: 40}));
+
+// STEP 10
+// alert("Hello " + "Volha Molash" + ", welcome to the 644 class!");
+
+// STEP 11
+// var name = "Volha Molash";
+// alert("Hello "+ name + ", welcome to the 644 class!");
+
+// STEP 12
+// var course = "JavaScript";
+// var name = "Olya Molash";
+// alert("Hello " + name + ", welcome to the " + course + " 644 class!");
+
+// STEP 13
+// var name = "Volha Molash";
+// alert("Hello " + name + ".\n" + "Welcome to the 644 class!");
+
+// STEP 14
+// var nameUser = prompt("Please enter your name");
+// alert("Hello " + nameUser + ".\n" + "Welcome to the 644 class!");
+
+// STEP 15
+// var nameUser = prompt("Please enter your name");
+// var courseUser = prompt("Please enter your class");
+// alert("Hello " + nameUser + ".\n" + "Welcome to the " + courseUser + " class!");
+
+// STEP 16
+// x = 10; //assign
+// y = 20;
+// console.log(x + y);
+
+// STEP 17
+// var x = 20; //declare (with var) and assign (with = )
+// console.log(x += 20);
+
+// STEP 18
+// var x = 20;
+// console.log( x *= 5);
+
+// STEP 19
+// var x = 8;
+// console.log(++x);
+
+// STEP 20
+// var arr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"];
+// console.log(arr);
+
